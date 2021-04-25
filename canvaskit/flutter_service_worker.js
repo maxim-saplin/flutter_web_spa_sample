481484fc70702f54ad21e88612c9da68d8cae9ec
maxim-saplin <smaxmail@gmail.com>
2021-04-25 17:45:55 +0000

    Deploying to gh-pages from @ maxim-saplin/flutter_web_spa_sample@ccc942709b88833505dfa222b538c6ab66e16fe9 🚀

--- canvaskit/flutter_service_worker.js
+++ canvaskit/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "d484a48997c4d67ae5d4b6223ddcfa6e",
-"index.html": "de8a5a0907c9234b09e7aabd0c499d55",
-"/": "de8a5a0907c9234b09e7aabd0c499d55",
+"index.html": "1266f2011266d49227e9067d34f6456a",
+"/": "1266f2011266d49227e9067d34f6456a",
 "main.dart.js": "55c0128c9c66f2c53cab2cfaff4f31c9",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
