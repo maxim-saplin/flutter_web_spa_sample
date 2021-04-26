bba55b4fa8bd7951ad09809d9fbff000fec2f298
Maxim Saplin <smaxmail@gmail.com>
2021-04-26 10:50:45 +0300

    Update flutter_service_worker.js

--- canvaskit/flutter_service_worker.js
+++ canvaskit/flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "d484a48997c4d67ae5d4b6223ddcfa6e",
 "index.html": "ca2883e62a3dfedc2a9056ae53f40003",
-"/": "ca2883e62a3dfedc2a9056ae53f40003",
+"": "ca2883e62a3dfedc2a9056ae53f40003",
 "main.dart.js": "55c0128c9c66f2c53cab2cfaff4f31c9",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -23,7 +23,7 @@ const RESOURCES = {
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = [
-  "/",
+  "",
 "main.dart.js",
 "index.html",
 "assets/NOTICES",
